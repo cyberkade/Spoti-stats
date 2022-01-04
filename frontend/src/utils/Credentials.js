4e3193c3632ad28ebbc8a825eca68c267aae7532
Kade Griffith <kadeg95@gmail.com>
2022-01-03 18:38:07 -0700

    cleans up code

--- frontend/src/utils/Credentials.js
+++ frontend/src/utils/Credentials.js
@@ -1,7 +1,7 @@
 // TODO: Figure out why .env file isn't connecting with the app
 const Credentials = {
   clientId: process.env.CLIENT_ID || "a4d359510d674b32af2ac4ff821e067d",
-  clientSecret: process.env.CLIENT_SECRET || "21fa2d7d75e6476baeea0e891c0fec2c",
+  clientSecret: process.env.CLIENT_SECRET || "",
 };
 
 export default Credentials;
--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ app.post("/refresh", (req, res) => {
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000",
     clientId: process.env.CLIENT_ID || "a4d359510d674b32af2ac4ff821e067d",
-    clientSecret:
-      process.env.CLIENT_SECRET || "21fa2d7d75e6476baeea0e891c0fec2c",
+    clientSecret: process.env.CLIENT_SECRET || "",
     refreshToken,
   });
 
