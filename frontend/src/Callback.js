029306309e441f96ba48b17b5bd6bdb7ea74befc
Kade Griffith <kadeg95@gmail.com>
2022-01-13 12:52:13 -0700

    heroku deploy v21

--- frontend/src/Callback.js
+++ frontend/src/Callback.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useContext } from "react";
 import { useSearchParams } from "react-router-dom";
 import useAuth from "./Hooks/useAuth";
-import axiosWithAuth from "../Utils/axiosWithAuth";
-import Dashboard from "../Components/Dashboard.js";
-import Loading from "../Common/Loading.js";
-import { TopArtistsContext } from "../Contexts/TopArtistsContext";
-import { TopTracksContext } from "../Contexts/TopTracksContext";
+import axiosWithAuth from "./Utils/axiosWithAuth";
+import Dashboard from "./Components/Dashboard.js";
+import Loading from "./Common/Loading.js";
+import { TopArtistsContext } from "./Contexts/TopArtistsContext";
+import { TopTracksContext } from "./Contexts/TopTracksContext";
 
 const Callback = () => {
   const { topArtists, setTopArtists } = useContext(TopArtistsContext);
